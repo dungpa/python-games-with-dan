dae02e18e5784c5fe51c040ef3ef95dc71fd93b8
Anh-Dung Phan <ADPH@simcorp.com>
2024-12-13 20:55:48 +0000

    Support screaming zapdos

--- shoot-the-bird/shoot.py
+++ shoot-the-bird/shoot.py
@@ -6,24 +6,29 @@ message = "Click the bird!"
 
 def draw():
     screen.clear()
-    screen.draw.text(message, topleft=(25,25))
+    x = randint (10, 50)
+    y = randint (10, 50)
+    screen.draw.text(message, topleft=(x, y))
     zapdos.draw()
+    
     
 def place_zapdos():
     zapdos.x = randint(10, 800)
     zapdos.y = randint(10, 600)
 
+def reset_zapdos_image():
+    zapdos.image = "zapdos"
+
 def on_mouse_down(pos):
     global message  # Declare `message` as global
     if zapdos.collidepoint(pos):
         message = "Good shot!"
-        sounds.squawk.play() 
+        sounds.squawk.play()
+        zapdos.image = "zapdoscream"
+        clock.schedule(reset_zapdos_image, 0.5)  # Reset image after 0.5 seconds
         place_zapdos()
     else:
         message = "Sorry, player, but you missed!"
-        clock.schedule(quit_game, 2)  # Schedule the quit after 2 seconds
-
-def quit_game():
-    quit()
+        clock.schedule(quit, 2)  # Schedule the quit after 2 seconds
 
 place_zapdos()
