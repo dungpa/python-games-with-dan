c8fb1053739b7be3ddc3d4d824698afd713cac3f
Anh-Dung Phan <ADPH@simcorp.com>
2024-12-13 21:04:10 +0000

    Keep the score

--- shoot-the-bird/shoot.py
+++ shoot-the-bird/shoot.py
@@ -3,14 +3,15 @@ import time
 
 zapdos = Actor("zapdos")
 message = "Click the bird!"
+score = 0
 
 def draw():
     screen.clear()
     x = randint (10, 50)
     y = randint (10, 50)
     screen.draw.text(message, topleft=(x, y))
-    zapdos.draw()
-    
+    screen.draw.text("Score: " + str(score), topleft=(5, 5))
+    zapdos.draw()    
     
 def place_zapdos():
     zapdos.x = randint(10, 800)
@@ -21,8 +22,10 @@ def reset_zapdos_image():
 
 def on_mouse_down(pos):
     global message  # Declare `message` as global
+    global score
     if zapdos.collidepoint(pos):
         message = "Good shot!"
+        score = score + 1
         sounds.squawk.play()
         zapdos.image = "zapdoscream"
         clock.schedule(reset_zapdos_image, 0.5)  # Reset image after 0.5 seconds
