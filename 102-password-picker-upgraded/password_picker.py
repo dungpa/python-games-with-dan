a9b0e1bf24d8cde5d177ebe138e880052bbc5047
Anh-Dung Phan <ADPH@simcorp.com>
2024-12-30 10:40:12 +0000

    Add colors to the passwords

--- 102-password-picker-upgraded/password_picker.py
+++ 102-password-picker-upgraded/password_picker.py
@@ -1,7 +1,9 @@
 import random
 import string
 
-adjectives = ['sleepy', 'slow', 'smelly', 'wet', 'fat', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'fluffy', 'white', 'proud', 'brave', 'sleek', 'intimidating', 'scary', 'cool', 'flying', 'spiky']
+adjectives = ['sleepy', 'slow', 'smelly', 'wet', 'fat', 'fluffy', 'proud', 'brave', 'sleek', 'intimidating', 'scary', 'cool', 'flying', 'spiky']
+
+colours = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'brown', 'black', 'white']
 
 nouns = ['apple', 'dinosaur', 'ball', 'toaster', 'goat', 'dragon', 'hammer', 'duck', 'panda', 'shark', 'kaiju', 'fish', 'pokemon', 'lion', 'lizard', 'snake']
 
@@ -9,11 +11,12 @@ print('Welcome to Password Picker!')
 
 while True:
     adjective = random.choice(adjectives)
+    colour = random.choice(colours)
     noun = random.choice(nouns)
     number = random.randrange(0, 100)
     special_char = random.choice(string.punctuation)
 
-    password = adjective + noun + str(number) + special_char
+    password = adjective + colour + noun + str(number) + special_char
     print('Your new password is: %s' % password)
     
     response = input('Would you like another password? Type y or n: ')
