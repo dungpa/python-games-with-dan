c6ede5c8430703db275ce4428b1af68b1333097b
Anh-Dung Phan <ADPH@simcorp.com>
2025-01-05 13:50:24 +0000

    Refactor

--- 108-countdown-calendar-expanded/countdown_calendar.py
+++ 108-countdown-calendar-expanded/countdown_calendar.py
@@ -16,25 +16,30 @@ def days_between_dates(date1, date2):
     time_between = str(date1 - date2)
     number_of_days = time_between.split(' ')
     return number_of_days[0]
-        
-root = Tk()
-c = Canvas(root, width=800, height=800, bg='black')
-c.pack()
-c.create_text(100, 50, anchor='w', fill='orange', font='Times 28 bold underline', text='My Countdown Calendar')
-
-events = get_events()
-today = date.today()
-
-vertical_space = 100
-
-for event in events:
-    event_name = event[0]
-    days_until = days_between_dates(event[1], today)
-    display = 'It is %s days until %s.' % (days_until, event_name)
-    if (int(days_until) <= 7):
-        text_col = 'red'
-    else:
-        text_col = 'lightblue'
-    c.create_text(100, vertical_space, anchor='w', fill=text_col, font='Terminal 28 bold', text=display)
     
-    vertical_space = vertical_space + 30
+def create_canvas():      
+    root = Tk()
+    c = Canvas(root, width=800, height=800, bg='black')
+    c.pack()
+    c.create_text(100, 50, anchor='w', fill='orange', font='Times 28 bold underline', text='My Countdown Calendar')
+    return c 
+    
+def display_events(events, c):
+    today = date.today()
+    vertical_space = 100
+
+    for event in events:
+        event_name = event[0]
+        days_until = days_between_dates(event[1], today)
+        display = 'It is %s days until %s.' % (days_until, event_name)
+        if (int(days_until) <= 7):
+            text_col = 'red'
+        else:
+            text_col = 'lightblue'
+        c.create_text(100, vertical_space, anchor='w', fill=text_col, font='Terminal 28 bold', text=display)
+        
+        vertical_space = vertical_space + 30
+    
+events = get_events()
+c = create_canvas()
+display_events(events, c)
