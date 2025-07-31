73134a0da4fe268d27fd2dbae49c477c3bbd3cd6
Anh-Dung Phan <phananhdung309@yahoo.com>
2025-07-31 18:40:29 +0100

    It's raining badguys

--- 203-space-invaders/space_invaders.py
+++ 203-space-invaders/space_invaders.py
@@ -1,4 +1,4 @@
-import pygame, sys, random
+import pygame, sys, random, time
 from pygame.locals import *
 pygame.init()
 clock = pygame.time.Clock()
@@ -6,35 +6,63 @@ pygame.display.set_caption("Space Invaders")
 screen = pygame.display.set_mode((640, 650))
 
 badguy_image = pygame.image.load("badguy.png").convert()
+badguy_image.set_colorkey((0, 0, 0))
+
+last_badguy_spawn_time = 0
 
 
 class Badguy:
     def __init__(self):
         self.x = random.randint(0, 570)
-        self.y = -45
+        self.y = -100
+        self.dy = random.randint(2, 6)
+        self.dx = random.choice((-1, 1))*self.dy
         
     def move(self):
-        self.y += 5
-        if self.y > 150 and self.y < 250:
-            self.x += 5
-        if self.y > 250:
-            self.x -= 5
+        if self.x < 0 or self.x > 570:
+            self.dx *= -1
+        self.x += self.dx
+        self.y += self.dy
         
     def draw(self):
         screen.blit(badguy_image, (self.x, self.y))
+    
+    def bounce(self):
+        if self.x < 0 or self.x > 570:
+            self.dx *= -1
+            
+    def off_screen(self):
+        return self.y > 640
+        if badguy.off_screen():
+            badguy = Badguy()
         
 badguy = Badguy()
+badguys = []
 
 while True:
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == QUIT:
             sys.exit
+    if time.time() - last_badguy_spawn_time > 0.5:
+        badguys.append(Badguy())
+        last_badguy_spawn_time = time.time()
+    
             
     screen.fill((0, 0, 0))
     
     badguy.move()
+    badguy.bounce()
     badguy.draw()
     
+    i = 0
+    while i < len(badguys):
+        badguys[i].move()
+        badguys[i].bounce()
+        badguys[i].draw()
+        if badguys[i].off_screen():
+            del badguys[i]
+            i -= 1
+        i += 1
     
     pygame.display.update()
