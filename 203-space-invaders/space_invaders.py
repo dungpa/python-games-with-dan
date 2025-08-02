f689ed662c4f89a0ed42031e97820919c51f3f82
Anh-Dung Phan <phananhdung309@yahoo.com>
2025-08-02 11:55:26 +0100

    some more additions

--- 203-space-invaders/space_invaders.py
+++ 203-space-invaders/space_invaders.py
@@ -7,6 +7,10 @@ screen = pygame.display.set_mode((640, 650))
 
 badguy_image = pygame.image.load("badguy.png").convert()
 badguy_image.set_colorkey((0, 0, 0))
+fighter_image = pygame.image.load("fighter.png").convert()
+fighter_image.set_colorkey((255, 255, 255))
+missile_image = pygame.image.load("missile.png").convert()
+missile_image.set_colorkey((255, 255, 255))
 
 last_badguy_spawn_time = 0
 
@@ -33,17 +37,52 @@ class Badguy:
             
     def off_screen(self):
         return self.y > 640
-        if badguy.off_screen():
-            badguy = Badguy()
-        
+
+class Fighter:
+    def __init__(self):
+        self.x = 320
+
+    def move(self):
+        if pressed_keys[K_LEFT] and self.x > 0:
+            self.x -= 3
+        if pressed_keys[K_RIGHT] and self.x < 540:
+            self.x += 3
+
+    def draw(self):
+        screen.blit(fighter_image, (self.x, 591))
+
+    def fire(self):
+        missiles.append(Missile(self.x + 50))
+
+class Missile:
+    def __init__(self,x):
+        self.x = x
+        self.y = 591
+
+    def move(self):
+        self.y -= 5
+
+    def off_screen(self):
+        return self.y < -8
+
+    def draw(self):
+        screen.blit(missile_image, (self.x - 4, self.y))
+
+
+
 badguy = Badguy()
 badguys = []
+fighter = Fighter()
+missiles = []
 
 while True:
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == QUIT:
-            sys.exit
+            sys.exit()
+        if event.type == KEYDOWN and event.key == K_SPACE:
+            fighter.fire()
+    pressed_keys = pygame.key.get_pressed()
     if time.time() - last_badguy_spawn_time > 0.5:
         badguys.append(Badguy())
         last_badguy_spawn_time = time.time()
@@ -54,6 +93,9 @@ while True:
     badguy.move()
     badguy.bounce()
     badguy.draw()
+
+    fighter.move()
+    fighter.draw()
     
     i = 0
     while i < len(badguys):
@@ -62,6 +104,15 @@ while True:
         badguys[i].draw()
         if badguys[i].off_screen():
             del badguys[i]
+            i -= 1
+        i += 1
+
+    i = 0
+    while i < len(missiles):
+        missiles[i].move()
+        missiles[i].draw()
+        if missiles[i].off_screen():
+            del missiles[i]
             i -= 1
         i += 1
     
